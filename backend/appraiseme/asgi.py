b01537a776b2562280d6ae417b8fc2e3a6d438a8
glory <gloryugwu04@gmail.com>
2025-03-28 00:55:32 -0400

    Pivot app to use django framework

---
+++ backend/appraiseme/asgi.py
@@ -0,0 +1,16 @@
+"""
+ASGI config for appraiseme project.
+
+It exposes the ASGI callable as a module-level variable named ``application``.
+
+For more information on this file, see
+https://docs.djangoproject.com/en/5.1/howto/deployment/asgi/
+"""
+
+import os
+
+from django.core.asgi import get_asgi_application
+
+os.environ.setdefault("DJANGO_SETTINGS_MODULE", "appraiseme.settings")
+
+application = get_asgi_application()
